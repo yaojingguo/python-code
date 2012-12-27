3642bd190bcd9d358a43f51673d1622d851d3f39
Jingguo Yao <yaojingguo@gmail.com>
2012-12-27 14:29:26 +0800

    Add

---
+++ rc4.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+A = ord('a')
+
+def info_num(text):
+  for c in text:
+    print(ord(c) - A, " ", end="")
+  print()
+
+def keystream(plain, key):
+  i = 0
+  length = len(plain)
+  result = ""
+  while i < length:
+    byte = (ord(plain[i]) - A + ord(key[i % 26]) - A) % 26
+    print(byte, " ", end="")
+    result += chr(byte + A)
+    i = i + 1
+  print()
+  print("text: ", result)
+
+def KSA(key):
+  S = [c for c in range(256)]
+  j = 0
+  keylength = len(key)
+  for i in range(256):
+    j = (j + S[i] + key[i % keylength]) % 256
+    S[i], S[j] = S[j], S[i]
+  return S
+
+def PRGA(data, S):
+  i = 0
+  j = 0
+  m = 0
+  length = len(data)
+  while m < length:
+    i = (i + 1) % 256
+    j = (j + S[i]) % 256
+    S[i], S[j] = S[j], S[i]
+    k = data[m] ^ S[(S[i] + S[j]) % 256]
+    print("%02X" % k, end="")
+    m += 1;
+  print()
+
+def rc4(data_text, key_text):
+  data = [ord(c) for c in data_text]
+  key = [ord(c) for c in key_text]
+  S = KSA(key)
+  PRGA(data, S)
+
+
+def test_keystream():
+  plain = "attackatdawn"
+  key = "kjcngmlhylyu"
+  info_num(plain)
+  info_num(key)
+  keystream(plain, key)
+
+def test_KSA():
+  key = [97, 98, 99, 100, 101]
+  S = KSA(key)
+  print(S)
+
+def test_rc4():
+  rc4("Plaintext", "Key")
+
+test_keystream()
+test_KSA()
+test_rc4()
