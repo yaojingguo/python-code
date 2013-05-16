86a1090462d879e3669eb73fd807160092dd5791
Jingguo Yao <yaojingguo@gmail.com>
2013-05-16 15:19:24 +0800

    Add

---
+++ MerkleTree.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import hashlib
+import unittest
+
+block_size = 10
+
+
+def hash(data):
+  sha1 = hashlib.sha1()
+  sha1.update(data)
+  return sha1.digest()
+
+def build(pathname):
+  # Compute hash for data blocks
+  f = open(pathname)
+  content = f.read()
+  length = len(content)
+  block_no = 0
+  while offset < length:
+    begin = block_no * block_size
+    end = (block_no + 1) * block_size
+    if end > length:
+      end = length
+    digest[block_no] = hash(content[begin:end])
+
+  # Build the binary tree
+
+  
+
+
+class Test(unittest.TestCase):
+  def test_hash(self):
+    data = "01234567890123456789"
+    digest = hash(data)
+
+
+
+if __name__ == "__main__":
+  unittest.main()
