2065bf37fc60af57ff68560d719da7d5b77ef570
Jingguo Yao <yaojingguo@gmail.com>
2013-05-19 10:21:20 +0800

    Finish Merkle tree

--- MerkleTree/MerkleTree.py
+++ MerkleTree/MerkleTree.py
@@ -1,44 +1,52 @@
 #!/usr/bin/env python
-
 import hashlib
 import unittest
 
-block_size = 10
+# Play with Merkle tree concepts.
 
+block_size = 10
 
 def hash(data):
   sha1 = hashlib.sha1()
   sha1.update(data)
+  print sha1.hexdigest()
   return sha1.digest()
 
+# The height of the Merkle tree is 1. It is like a hash list.
 def build(pathname):
+  print("file: %s" % pathname)
   # Compute hash for data blocks
   f = open(pathname)
   content = f.read()
   length = len(content)
   block_no = 0
-  while offset < length:
+  digest = []
+  print "Digest:"
+  while (block_no * block_size) < length:
     begin = block_no * block_size
     end = (block_no + 1) * block_size
     if end > length:
       end = length
-    digest[block_no] = hash(content[begin:end])
-    print str(digest[block_no]).encode("hex")
+    digest.append(hash(content[begin:end]))
+    block_no += 1
 
   # Build the binary tree
+  top = []
   for i in range(20):
-    top[i] = 0
+    byte = 0
     for finger in digest:
-      top[i] = top[i] ^ finger[i]
-  
+      byte = byte ^ ord(finger[i])
+    top.append(byte)
+  str = ""
+  for byte in top:
+    str += hex(byte)[2:]
+  print "top: ", str
 
 
 class Test(unittest.TestCase):
   def test_hash(self):
-    data = "01234567890123456789"
-    digest = hash(data)
-
-
+    build("data_1")
+    build("data_2")
 
 if __name__ == "__main__":
   unittest.main()
